83136ef784cee95c2edf36dabff518d8d5c530b1
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2024-04-04 14:50:31 -0400

    Latest aws_backup.sh, still testing

--- aws_backup.sh
+++ aws_backup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo "$AWS_ACCESS_KEY"
-echo "$AWS_SECRET_ACCESS_KEY"
+# echo "$AWS_ACCESS_KEY"
+# echo "$AWS_SECRET_ACCESS_KEY"
 
-OLDER_THAN="3M"
+export PASSPHRASE=$DUPLICITY_ENCRYPTER_PASSWORD
+export SIGN_PASSPHRASE=$DUPLICITY_SIGNER_PASSWORD
+
+OLDER_THAN="1h"
 
 MONTH_NUM="$(/bin/date +%m)"
 MONTH_NAME="$(date +%b)"
@@ -12,8 +15,14 @@ DAY="$(/bin/date +%d)"
 DESTINATION="s3://duplicity.gautier.org/$MONTH_NUM-$MONTH_NAME/"
 echo "$DESTINATION"
 
-is_running=$(ps -ef | grep duplicity  | grep python | wc -l)
+# is_running=$(ps -ef | grep duplicity  | grep python | wc -l)
 
 
 /usr/bin/duplicity remove-older-than ${OLDER_THAN} ${DESTINATION}
 #  >> ${DAILYLOGFILE} 2>&1
+
+/usr/bin/duplicity \
+ --archive-dir $HOME/tmp \
+ --encrypt-key $DUPLICITY_ENCRYPTER_FINGERPRINT \
+ --sign-key $DUPLICITY_SIGNER_FINGERPRINT \
+ --verbosity d $HOME/test $DESTINATION
